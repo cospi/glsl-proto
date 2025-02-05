02e8a11b236774791c91fa061529f562aef1f8cf
Aleksanteri Hirvonen <20629710+cospi@users.noreply.github.com>
2025-02-05 20:38:57 +0200

    Unify parameter naming

--- src/math/rect2.h
+++ src/math/rect2.h
@@ -8,14 +8,14 @@ typedef struct Rect2 {
 	Vector2 size;
 } Rect2;
 
-static inline void rect2_min_max(const Rect2 *rect, Vector2 *out_min, Vector2 *out_max)
+static inline void rect2_min_max(const Rect2 *_this, Vector2 *out_min, Vector2 *out_max)
 {
-	assert(rect != NULL);
+	assert(_this != NULL);
 	assert(out_min != NULL);
 	assert(out_max != NULL);
 
-	*out_min = rect->position;
-	*out_max = vector2_sum(&rect->position, &rect->size);
+	*out_min = _this->position;
+	*out_max = vector2_sum(&_this->position, &_this->size);
 }
 
 #endif // MATH_RECT2_H_
